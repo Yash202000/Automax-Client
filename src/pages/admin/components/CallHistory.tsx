acfeb1ad7624ec6a7e1c136509881862523aca67
LeaderHisham <a.hisham@leadergroup.com>
2026-09-10 14:52:10 +0530

    ..

--- src/pages/admin/components/CallHistory.tsx
+++ src/pages/admin/components/CallHistory.tsx
@@ -361,7 +361,6 @@ const CallHistoryItem: React.FC<{
                     : "text-slate-400"
               }`,
             })}
-            Compose and send an email
           </div>
           <div className="flex-1 min-w-0">
             <div className="flex items-center gap-2">
