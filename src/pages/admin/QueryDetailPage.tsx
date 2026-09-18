6d21e6488ac5e3f5b7001f1b3be923ff4c5698b3
Amal Dev <d.amal@leadergroup.com>
2026-09-18 10:47:59 +0530

    fix

--- src/pages/admin/QueryDetailPage.tsx
+++ src/pages/admin/QueryDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import { publicUrl } from "../../utils/publicUrl";
 import { getOsmTileUrl } from "../../utils/osmTileUrl";
 import { ZoomableImage } from "../../components/common/ZoomableImage";
